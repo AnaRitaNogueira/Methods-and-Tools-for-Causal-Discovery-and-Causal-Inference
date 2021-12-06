146ff15b0b9b31f23dc1b7df8f2690e925e4f2b3
AnaRitaNogueira <59921770+AnaRitaNogueira@users.noreply.github.com>
2021-12-06 16:39:54 +0000

    Update pattern comparation.R

--- 3. Causal Discovery/3.1 Finding causal relations in cross-sectional data/pattern comparation.R
+++ 3. Causal Discovery/3.1 Finding causal relations in cross-sectional data/pattern comparation.R
@@ -1,6 +1,8 @@
 
 library(bnlearn)
 library(pcalg)
+
+
 numeric_asia <- data_frame_to_numeric(asia)
 values <-
   as.numeric(countUniqueValues(numeric_asia))
@@ -21,32 +23,7 @@ pc.fit <- pc(
   verbose = TRUE
 )
 
-pc.fit<-as.bn(pc.fit)
 
+
+#implementation of bnlearn's PC
 pc<-pc.stable(x=asia,alpha = 0.05,test="x2")
-
-score <- new("GaussL0penObsScore", numeric_asia)
-ges.fit <- ges(score)
-ges.fit<-essgraph_to_graphNEL(ges.fit)
-
-fci.fit <- fci(
-  suffStat,
-  indepTest = binCItest,
-  alpha = 0.05,
-  labels = V,
-  # u2pd = "retry",
-  verbose = TRUE
-)
-plot(fci.fit)
-
-fci.fit<-amat_to_graphNEL(fci.fit@amat)
-
-true_dag = model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]")
-graphviz.plot(true_dag)
-
-png("/home/rita/Documents/Micro-Projecto Causalidade/Examples for the paper/comparision.png",width = 1000,height = 1000)
-par(mfrow = c(1,1),bg=NA)
-graphviz.compare(true_dag, pc.fit)
-dev.off()
-
-graphviz.compare(dag,x)
