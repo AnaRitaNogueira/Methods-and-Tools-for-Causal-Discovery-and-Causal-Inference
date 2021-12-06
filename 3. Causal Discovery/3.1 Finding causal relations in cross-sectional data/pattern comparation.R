362965f8768bf5a8615f3aac88e10b076af92e09
AnaRitaNogueira <59921770+AnaRitaNogueira@users.noreply.github.com>
2021-12-06 16:45:19 +0000

    Update pattern comparation.R

--- 3. Causal Discovery/3.1 Finding causal relations in cross-sectional data/pattern comparation.R
+++ 3. Causal Discovery/3.1 Finding causal relations in cross-sectional data/pattern comparation.R
@@ -27,3 +27,12 @@ pc.fit <- pc(
 
 #implementation of bnlearn's PC
 pc<-pc.stable(x=asia,alpha = 0.05,test="x2")
+
+
+#comparision between the true network and the generated networks (since, the three methods generated the same network, we compare only one)
+
+true_dag = model2network("[A][S][T|A][L|S][B|S][D|B:E][E|T:L][X|E]")
+graphviz.plot(true_dag)
+graphviz.compare(true_dag, pc.fit)
+
+DAG_comparision(true_dag, pc.fit)
